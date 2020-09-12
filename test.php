aa4cfa07be6809e432ae12b3a5185c74db13f3b5
AndrewScottPH <AndrewScottPH@gmail.com>
2020-09-12 19:51:18 +0300

    add READMY.MD

--- test.php
+++ test.php
@@ -1,1 +1,9 @@
 <?php
+require_once 'scripts/database_connection.php';
+
+
+//$login = 'Jess';
+//$password = password_hash('qwerty', PASSWORD_BCRYPT);
+//$label = '';
+//$query = sprintf("INSERT INTO users (login, password, label) VALUE ('%s','%s','%s')", $login, $password, $label);
+//$mysqli->query($query);
